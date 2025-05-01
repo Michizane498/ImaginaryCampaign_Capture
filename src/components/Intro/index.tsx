64aa20b76168471e595870e86bc94ae78c6be671
Michizane498 <141784707+Michizane498@users.noreply.github.com>
2025-05-02 00:14:05 +0300

    Images

--- src/components/Intro/index.tsx
+++ src/components/Intro/index.tsx
@@ -1,29 +1,59 @@
 "use client";
 
-import React, { useEffect } from "react";
+import React, { useLayoutEffect, useRef } from "react";
 import styles from "./page.module.css";
 import Image from "next/image";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+import gsap from "gsap";
 export default function index() {
-  useEffect(() => {
-    (async () => {
-      const LocomotiveScroll = (await import("locomotive-scroll")).default;
-      const locomotiveScroll = new LocomotiveScroll();
-    })();
+  const backgroundImage = useRef<HTMLDivElement>(null);
+  const introImage = useRef<HTMLDivElement>(null);
+
+  useLayoutEffect(() => {
+    gsap.registerPlugin(ScrollTrigger);
+
+    const timeline = gsap.timeline({
+      scrollTrigger: {
+        trigger: document.documentElement,
+        start: "top top",
+        end: "+=500px",
+        scrub: true,
+        markers: true,
+        toggleActions: "play none none reverse",
+      },
+    });
+
+    timeline
+    .fromTo(
+      backgroundImage.current,
+      { clipPath: "inset(15%)" }, // start state
+      { clipPath: "inset(0%)" }   // end state
+    )
+      .to(introImage.current, { height: "120px" }, 0);
+
   }, []);
+
   return (
     <div className={styles.intro}>
-      <div className={styles.backgroundImage}>
+      <div ref={backgroundImage} className={styles.backgroundImage}>
         <Image
           alt="Background image"
           fill={true}
-          src={"/reddarkbackground.jpg"}
+          src={"/back6.jpg"}
         />
       </div>
       <div className={styles.introContainer}>
-        <div data-scroll data-scroll-speed="0.3" className={styles.introImage}>
-          <Image alt="Background image" fill={true} src={"/aura.jpg"} />
+        <div
+          ref={introImage}
+          data-scroll
+          data-scroll-speed="0.3"
+          className={styles.introImage}
+        >
+          <Image alt="Background image" fill={true} src={"/back7.jpg"} />
         </div>
-        <h1 data-scroll data-scroll-speed="0.7">Smooth Scroll</h1>
+        <h1 data-scroll data-scroll-speed="0.7">
+          Smooth Scroll
+        </h1>
       </div>
     </div>
   );
---
+++ src/components/scroll.tsx
@@ -0,0 +1,14 @@
+"use client";
+
+import { useEffect } from "react";
+
+export default function Layout({ children }: { children: React.ReactNode }) {
+  useEffect(() => {
+    (async () => {
+      const LocomotiveScroll = (await import("locomotive-scroll")).default;
+      const locomotiveScroll = new LocomotiveScroll();
+    })();
+  }, []);
+
+  return <main data-scroll-container>{children}</main>;
+}
