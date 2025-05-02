a35dfd709ffb9317ba14dfa44ab01e7dafd1fa61
Michizane498 <141784707+Michizane498@users.noreply.github.com>
2025-05-02 17:27:16 +0300

    Adjustment

--- src/components/scroll.tsx
+++ src/components/scroll.tsx
@@ -5,7 +5,7 @@ import { useEffect } from "react";
 export default function Layout({ children }: { children: React.ReactNode }) {
   useEffect(() => {
     (async () => {
-      const LocomotiveScroll = (await import("locomotive-scroll")).default;
+      (await import("locomotive-scroll")).default;
       // const locomotiveScroll = new LocomotiveScroll();
     })();
   }, []);
