838bfe1413bf0c6b4d7833317184d2f3333243a3
Michizane498 <141784707+Michizane498@users.noreply.github.com>
2025-05-02 14:27:30 +0300

    Third section

---
+++ src/components/Projects/index.tsx
@@ -0,0 +1,50 @@
+import React, { useState } from "react";
+import styles from "./page.module.css";
+import Image from "next/image";
+
+export default function index() {
+  const [selectedProject, setSelectedProject] = useState(0);
+
+  const projects = [
+    {
+      title: "Brutalist",
+      src: "/Brutalism.jpg",
+    },
+    {
+      title: "Glassmorphism",
+      src: "/Glass.jpg",
+    },
+    {
+      title: "Mesh Gradient",
+      src: "/MeshGradient.jpg",
+    },
+    {
+      title: "3D Timelines",
+      src: "/3dtimelines.jpg",
+    },
+  ];
+  return (
+    <div className={styles.projects}>
+      <div className={styles.projectDescription}>
+        <div className={styles.imageContainer}>
+          <Image src={projects[selectedProject].src} fill={true} alt="image" />
+        </div>
+        <div className={styles.column}>
+          <p>
+            Whether using a smartphone or a professional camera, the art lies in
+            framing a story within a single image. Patience, creativity, and
+            technical knowledge all come together to transform ordinary scenes
+            into lasting memories.
+          </p>
+        </div>
+        <div className={styles.column}>
+          <p>
+            While editing software can enhance colors, adjust exposure, and
+            remove flaws, the best photos start with strong fundamentals—sharp
+            focus, balanced composition, and thoughtful lighting.
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+}
